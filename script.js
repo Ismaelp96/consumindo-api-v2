90ceed453b24c0118ff259cd0ab097621c8860d2
ismaelp96 <ismael6991@live.com>
2024-01-22 12:08:08 -0300

    Async/Await

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const containerVideos = document.querySelector('.videos__container');
 
-const api = fetch(`http://localhost:3000/videos`)
-  .then((res) => res.json())
-  .then((videos) =>
-    videos.forEach((video) => {
-      containerVideos.innerHTML += `
+async function searchAndShowVideos() {
+  const search = await fetch(`http://localhost:3000/videos`);
+  const videos = await search.json();
+  videos.forEach((video) => {
+    containerVideos.innerHTML += `
             <li class="videos__item">
                 <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
                 <div class="descricao-video">
@@ -14,8 +14,7 @@ const api = fetch(`http://localhost:3000/videos`)
                 </div>
             </li>
         `;
-    })
-  )
-  .catch((error) => {
-    containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
   });
+}
+
+searchAndShowVideos();
